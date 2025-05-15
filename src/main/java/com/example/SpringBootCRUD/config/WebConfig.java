06aa4aaca418874e21efc9df67fee76526adfcbf
Jay-sorani835 <148628735+Jay-sorani835@users.noreply.github.com>
2025-05-15 16:56:43 +0530

    Update WebConfig.java

--- src/main/java/com/example/SpringBootCRUD/config/WebConfig.java
+++ src/main/java/com/example/SpringBootCRUD/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Define the CORS settings for your API endpoints
         registry.addMapping("/**")  // Apply to all paths
-                .allowedOrigins("https://ems-frontend-one-tawny.vercel.app/")  
+                .allowedOrigins("https://ems-frontend-one-tawny.vercel.app")  
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allowed HTTP methods
                 .allowedHeaders("*")  // Allow any headers
                 .allowCredentials(true);  // Allow credentials like cookies or auth tokens
